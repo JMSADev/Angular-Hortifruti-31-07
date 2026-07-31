f0e4a4329e04d1c58ee877f476495ce648c7dc00
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-31 19:49:44 -0300

    adicionando codigo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface Produto {
+  nome: string;
+  valor: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('controle-mercado');
+  //lista que vai guardar todos os produtos
+  produtos: Produto[] = [];
+  //Variáveis para conectar com os campos do html
+  nomeDigitado: string = '';
+  valorDigitado: number = 0;
+
+  adicionarProduto() {
+    //Cria um novo produto com valores digitados
+    const novoProduto: Produto = {
+      nome: this.nomeDigitado,
+      valor: this.valorDigitado
+    };
+
+    //Coloca o produto na lista
+    this.produtos.push(novoProduto);
+
+    //Limpa os campos da tela
+    this.nomeDigitado = '';
+    this.valorDigitado = 0;
+  }
 }
